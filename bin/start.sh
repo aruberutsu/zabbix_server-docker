8f075fc521758fc24f77fcef29e8b2a76fd408d0
aruberutsu <aruberutsu@users.noreply.github.com>
2021-12-11 14:21:42 +0100

    Provision database on first run
    Automatically populate database on first run, this avoiding the need to
    manually run commands in the zabbix container.

--- bin/start.sh
+++ bin/start.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+if test ! -e /.firstrun ; then
+  echo "Executing initial setup..."
+  sleep 30
+  echo create database zabbix character set utf8 collate utf8_bin | mysql -h zabbix-sql -u root -p123456789
+  echo grant all privileges on zabbix.* to zabbix identified by \'zabbix\' | mysql -h zabbix-sql -uroot -p123456789
+  echo flush privileges | mysql -h zabbix-sql -u root -p123456789
+  sleep 1
+  mysql -h zabbix-sql -uzabbix -pzabbix zabbix < /etc/zabbix/schema.sql
+  mysql -h zabbix-sql -uzabbix -pzabbix zabbix < /etc/zabbix/images.sql
+  mysql -h zabbix-sql -uzabbix -pzabbix zabbix < /etc/zabbix/data.sql
+  touch /.firstrun
+  echo "...done initial setup"
+fi
+
 # Start zabbix-server
 
 /usr/sbin/zabbix_server -c /etc/zabbix/zabbix_server.conf
@@ -8,7 +22,11 @@
 
 /usr/sbin/zabbix_agentd
 
+# Start php-fpm
+
+/usr/sbin/php-fpm
+
 # Start httpd
 
-apachectl -DFOREGROUND
+/usr/sbin/httpd -DFOREGROUND
 
